f86c2814442e27507adbe8c974bd2194075b075c
wangxq <ajesses891@gmail.com>
2023-03-22 16:37:49 +0800

    chore: 打包目录逻辑

---
+++ packages/vue3-drag-layout/README.md
@@ -0,0 +1,2 @@
+#### vue3-grid-layout
+This project rewrite with vue3 base on [vue-grid-layout](https://github.com/jbaysolutions/vue-grid-layout)
---
+++ packages/vue3-drag-layout/component.ts
@@ -0,0 +1,4 @@
+import type { Plugin } from 'vue'
+import { AtButton } from '@vue3-drag-layout/components'
+
+export default [AtButton] as Plugin[]
